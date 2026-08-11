f970cfe95f3ef67b6b4cb245af153b7f405af4f6
= <=>
2026-08-11 10:49:26 -0300

    adicionando leds

--- codigo_arduino/codigo_arduino_ldr/codigo_arduino_ldr.ino
+++ codigo_arduino/codigo_arduino_ldr/codigo_arduino_ldr.ino
@@ -8,10 +8,16 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
+int limite_baixo = 300; 
+int limite_alto = 600;
+
 void setup() {
   Serial.begin(9600);
 
   dht.begin();
+  pinMode(led_verde, OUTPUT);
+  pinMode(led_amarelo, OUTPUT); 
+  pinMode(led_vermelho, OUTPUT);
 }
 
 void loop() {
@@ -23,6 +29,22 @@ void loop() {
 
   // LDR
   int luz = analogRead(LDRPIN);
+
+// Apaga todos os LEDs primeiro 
+  digitalWrite(led_verde, LOW);
+  digitalWrite(led_amarelo, LOW); 
+  digitalWrite(led_vermelho, LOW);
+
+ 
+
+// Comparação da luminosidade 
+if (luz < limite_baixo) { 
+  digitalWrite(led_vermelho, HIGH); }
+else if (luz < limite_alto) { 
+  digitalWrite(led_amarelo, HIGH); }
+else { digitalWrite(led_verde, HIGH); }
+
+
 
   if (isnan(h) || isnan(t)) {
     Serial.println("Falha ao ler o DHT22!");
